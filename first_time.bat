e47ab3098b7cdb7018b4cfd5f08650cead6907b9
Jason Hung <jasonjei@gmail.com>
2016-08-16 23:58:27 -0700

    First time bat script works!

--- first_time.bat
+++ first_time.bat
@@ -1,11 +1,30 @@
+call "%VS110COMNTOOLS%"\vsvars32.bat
 set QUICKSLINGPATH=%~dp0
 if not exist "%QUICKSLINGPATH%\cef_binary\CURRENT_VERSION_3.2526.1347.gcf20046" (
+	GOTO dostuff
+)
+
+if not exist "%QUICKSLINGPATH%\cef_binary\Debug\libcef.dll" (
+	GOTO dostuff
+)
+
+if not exist "%QUICKSLINGPATH%\cef_binary\build\libcef_dll\Debug\libcef_dll_wrapper.lib" (
+	GOTO compilestuff
+)
+
+if not exist "%QUICKSLINGPATH%\cef_binary\build\libcef_dll\Release\libcef_dll_wrapper.lib" (
+	GOTO compilestuff
+)
+
+GOTO END
+
+:dostuff
 	if not exist "%QUICKSLINGPATH%\archives" (
 		mkdir "%QUICKSLINGPATH%\archives"
 	)
 
 	if not exist "%QUICKSLINGPATH%\archives\cef_binary_3.2526.1347.gcf20046_windows32.7z" ( 
-		%QUICKSLINGPATH%\tools\wget https://storage.googleapis.com/quicksling/build/cef_binary_3.2526.1347.gcf20046_windows32.7z -P %QUICKSLINGPATH%\archives\
+		"%QUICKSLINGPATH%\tools\wget" https://storage.googleapis.com/quicksling/build/cef_binary_3.2526.1347.gcf20046_windows32.7z -O "%QUICKSLINGPATH%archives\cef_binary_3.2526.1347.gcf20046_windows32.7z"
 	)
 	
 	"%QUICKSLINGPATH%\tools\7za.exe" x -y -o"%QUICKSLINGPATH%" "archives\cef_binary_3.2526.1347.gcf20046_windows32.7z" "cef_binary_3.2526.1347.gcf20046_windows32\Debug" "cef_binary_3.2526.1347.gcf20046_windows32\Release"
@@ -21,8 +40,11 @@ if not exist "%QUICKSLINGPATH%\cef_binary\CURRENT_VERSION_3.2526.1347.gcf20046" 
 		mkdir "%QUICKSLINGPATH%\cef_binary\build"
 	)
 
-	cmake -G "Visual Studio 12" "%QUICKSLINGPATH%\cef_binary\"
+	cmake -G "Visual Studio 12" -B"%QUICKSLINGPATH%\cef_binary\build" -H"%QUICKSLINGPATH%\cef_binary\"
 
 	set CMAKE_ERRORLEVEL=%ERRORLEVEL%
-	if "%CMAKE_ERRORLEVEL%" EQU 0 type nul >>"%QUICKSLINGPATH%\cef_binary\CURRENT_VERSION_3.2526.1347.gcf20046" & copy "%QUICKSLINGPATH%\cef_binary\CURRENT_VERSION_3.2526.1347.gcf20046" +,,
-)
+	if "%CMAKE_ERRORLEVEL%" EQU "0" type nul >> "%QUICKSLINGPATH%\cef_binary\CURRENT_VERSION_3.2526.1347.gcf20046" & copy "%QUICKSLINGPATH%\cef_binary\CURRENT_VERSION_3.2526.1347.gcf20046" +,,
+:compilestuff
+	msbuild "%QUICKSLINGPATH%\cef_binary\build\libcef_dll\libcef_dll_wrapper.vcxproj " /t:Build /p:Configuration=Release
+	msbuild "%QUICKSLINGPATH%\cef_binary\build\libcef_dll\libcef_dll_wrapper.vcxproj " /t:Build /p:Configuration=Debug
+:END
