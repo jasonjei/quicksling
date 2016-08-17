741dde0066435535b3558c562fc06029056e1f29
Jason Hung <jasonjei@gmail.com>
2016-08-16 23:13:42 -0700

    Changes to make repo not depend on Jason's setup

--- first_time.bat
+++ first_time.bat
@@ -1,23 +1,28 @@
-set QUICKSLINGPATH=%cd%
-if not exist "%cd%\cef_binary\CURRENT_VERSION_3.2526.1347.gcf20046" (
-	if not exist "%cd%\archives" (
-		mkdir archives
+set QUICKSLINGPATH=%~dp0
+if not exist "%QUICKSLINGPATH%\cef_binary\CURRENT_VERSION_3.2526.1347.gcf20046" (
+	if not exist "%QUICKSLINGPATH%\archives" (
+		mkdir "%QUICKSLINGPATH%\archives"
 	)
 
-	if not exist "archives\cef_binary_3.2526.1347.gcf20046_windows32.7z" ( 
-		tools\wget https://storage.googleapis.com/quicksling/build/cef_binary_3.2526.1347.gcf20046_windows32.7z
+	if not exist "%QUICKSLINGPATH%\archives\cef_binary_3.2526.1347.gcf20046_windows32.7z" ( 
+		%QUICKSLINGPATH%\tools\wget https://storage.googleapis.com/quicksling/build/cef_binary_3.2526.1347.gcf20046_windows32.7z -P %QUICKSLINGPATH%\archives\
 	)
+	
 	"%QUICKSLINGPATH%\tools\7za.exe" x -y -o"%QUICKSLINGPATH%" "archives\cef_binary_3.2526.1347.gcf20046_windows32.7z" "cef_binary_3.2526.1347.gcf20046_windows32\Debug" "cef_binary_3.2526.1347.gcf20046_windows32\Release"
 	robocopy "cef_binary_3.2526.1347.gcf20046_windows32\Debug" "%QUICKSLINGPATH%\cef_binary\Debug" /E /IS /MOVE
 	robocopy "cef_binary_3.2526.1347.gcf20046_windows32\Release" "%QUICKSLINGPATH%\cef_binary\Release" /E /IS /MOVE
-	rmdir /S /Q cef_binary_3.2526.1347.gcf20046_windows32
+	rmdir /S /Q "%QUICKSLINGPATH%\cef_binary_3.2526.1347.gcf20046_windows32"
 
-	if not exist "%cd%\cef_binary\build" (
-		mkdir "%cd%\cef_binary\build"
-		cd "%cd%\cef_binary\build"
-		cmake -G "Visual Studio 12" ..
+	if exist "%QUICKSLINGPATH%\cef_binary\build" (
+		rmdir /S /Q "%QUICKSLINGPATH%\cef_binary\build"
 	)
-	cd "%QUICKSLINGPATH%\cef_binary"
-	type nul >>CURRENT_VERSION_3.2526.1347.gcf20046 & copy CURRENT_VERSION_3.2526.1347.gcf20046 +,,
-	cd "%QUICKSLINGPATH%"
+
+	if not exist "%QUICKSLINGPATH%\cef_binary\build" (
+		mkdir "%QUICKSLINGPATH%\cef_binary\build"
+	)
+
+	cmake -G "Visual Studio 12" "%QUICKSLINGPATH%\cef_binary\"
+
+	set CMAKE_ERRORLEVEL=%ERRORLEVEL%
+	if "%CMAKE_ERRORLEVEL%" EQU 0 type nul >>"%QUICKSLINGPATH%\cef_binary\CURRENT_VERSION_3.2526.1347.gcf20046" & copy "%QUICKSLINGPATH%\cef_binary\CURRENT_VERSION_3.2526.1347.gcf20046" +,,
 )
