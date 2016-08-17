86aa976399eeb6e0ba9d01368c9c74222b259e14
Jason Hung <jasonjei@gmail.com>
2016-08-17 11:49:36 -0700

    Fixes for batch and prebuild script

--- first_time.bat
+++ first_time.bat
@@ -19,6 +19,10 @@ if not exist "%QUICKSLINGPATH%\cef_binary\build\libcef_dll\Release\libcef_dll_wr
 GOTO END
 
 :dostuff
+	if exist "%QUICKSLINGPATH%\cef_binary\CURRENT_VERSION_*" (
+		del /F /S "%QUICKSLINGPATH%\cef_binary\CURRENT_VERSION_*"
+	)
+
 	if not exist "%QUICKSLINGPATH%\archives" (
 		mkdir "%QUICKSLINGPATH%\archives"
 	)
@@ -27,9 +31,9 @@ GOTO END
 		"%QUICKSLINGPATH%\tools\wget" https://storage.googleapis.com/quicksling/build/cef_binary_3.2526.1347.gcf20046_windows32.7z -O "%QUICKSLINGPATH%archives\cef_binary_3.2526.1347.gcf20046_windows32.7z"
 	)
 	
-	"%QUICKSLINGPATH%\tools\7za.exe" x -y -o"%QUICKSLINGPATH%" "archives\cef_binary_3.2526.1347.gcf20046_windows32.7z" "cef_binary_3.2526.1347.gcf20046_windows32\Debug" "cef_binary_3.2526.1347.gcf20046_windows32\Release"
-	robocopy "cef_binary_3.2526.1347.gcf20046_windows32\Debug" "%QUICKSLINGPATH%\cef_binary\Debug" /E /IS /MOVE
-	robocopy "cef_binary_3.2526.1347.gcf20046_windows32\Release" "%QUICKSLINGPATH%\cef_binary\Release" /E /IS /MOVE
+	"%QUICKSLINGPATH%\tools\7za.exe" x -y -o"%QUICKSLINGPATH%" "%QUICKSLINGPATH%\archives\cef_binary_3.2526.1347.gcf20046_windows32.7z" "cef_binary_3.2526.1347.gcf20046_windows32\Debug" "cef_binary_3.2526.1347.gcf20046_windows32\Release"
+	robocopy "%QUICKSLINGPATH%\cef_binary_3.2526.1347.gcf20046_windows32\Debug" "%QUICKSLINGPATH%\cef_binary\Debug" /E /IS /MOVE
+	robocopy "%QUICKSLINGPATH%\cef_binary_3.2526.1347.gcf20046_windows32\Release" "%QUICKSLINGPATH%\cef_binary\Release" /E /IS /MOVE
 	rmdir /S /Q "%QUICKSLINGPATH%\cef_binary_3.2526.1347.gcf20046_windows32"
 
 	if exist "%QUICKSLINGPATH%\cef_binary\build" (
@@ -43,8 +47,8 @@ GOTO END
 	cmake -G "Visual Studio 12" -B"%QUICKSLINGPATH%\cef_binary\build" -H"%QUICKSLINGPATH%\cef_binary\"
 
 	set CMAKE_ERRORLEVEL=%ERRORLEVEL%
-	if "%CMAKE_ERRORLEVEL%" EQU "0" type nul >> "%QUICKSLINGPATH%\cef_binary\CURRENT_VERSION_3.2526.1347.gcf20046" & copy "%QUICKSLINGPATH%\cef_binary\CURRENT_VERSION_3.2526.1347.gcf20046" +,,
+	if "%CMAKE_ERRORLEVEL%" EQU "0" type nul >> "%QUICKSLINGPATH%\cef_binary\CURRENT_VERSION_3.2526.1347.gcf20046"
 :compilestuff
-	msbuild "%QUICKSLINGPATH%\cef_binary\build\libcef_dll\libcef_dll_wrapper.vcxproj " /t:Build /p:Configuration=Release
-	msbuild "%QUICKSLINGPATH%\cef_binary\build\libcef_dll\libcef_dll_wrapper.vcxproj " /t:Build /p:Configuration=Debug
+		msbuild "%QUICKSLINGPATH%\cef_binary\build\libcef_dll\libcef_dll_wrapper.vcxproj " /t:Build /p:Configuration=Debug
+		msbuild "%QUICKSLINGPATH%\cef_binary\build\libcef_dll\libcef_dll_wrapper.vcxproj " /t:Build /p:Configuration=Release
 :END
